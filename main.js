b9256c9b5d13b4ad82729e00d300c0b1836e078b
JasmineGuy <guyjasmine.c@gmail.com>
2021-08-09 08:41:26 -0500

    new notes

--- main.js
+++ main.js
@@ -10,12 +10,61 @@
 
 
 
-let message = 'Hello'
+// let message = 'Hello'
 
-if(message ==='Hello'){
-    let message ="Inside of if block"
-    console.log(message)
-}
-console.log(message)
+// if(message ==='Hello'){
+//     let message ="Inside of if block"
+//     console.log(message)
+// }
+// console.log(message)
 
-//Block scope example above. Block = anything contained in the curly braces of  an if statment, a while or a for loop, or any set of curly braces other than a function
+//Block scope example above. Block = anything contained in the curly braces of  an if statement, a while or a for loop, or any set of curly braces other than a function
+
+
+// variables declared with var keyword or within  function declarations DO NOT have block scope
+
+// Immediately invokes Function Expression (IIFE)
+
+// Function Expression: Define a function and assign it to a variable
+
+// Immediately invoked: invoking the function right away where it's defined
+
+
+// Function
+// function greeting(){
+//     console.log('Hello')
+// }
+// greeting();
+
+
+// IIFE
+
+// (function() {
+//     console.log('Hello');
+// })();
+
+// Notice the IIFE version doesn't have a name and it is completely inside parenthesis. It also is invoked at the end. This function is immediately invoked
+
+
+
+// Closures
+// let greeting = (function(){
+//     let message = 'Hello';
+//     let getMessage = function(){
+//         return message;
+//     };
+// })();
+// console.log(greeting.message)
+//undefined because message variable is out of scope. You couls use closures to prevent the necessary variables from going out of scope
+
+// ex:
+let greeting =(function(){
+    let message = 'Hello'
+    let getMessage = function(){
+        return message;
+    };
+    return {
+        getMessage: getMessage,
+    };
+})();
+console.log(greeting.getMessage());
