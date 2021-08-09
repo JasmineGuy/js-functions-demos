67af760e4b96865a4bfbe23f7e9a781e3b22e1fd
JasmineGuy <guyjasmine.c@gmail.com>
2021-08-09 09:31:45 -0500

    notes on arrow functions

--- main.js
+++ main.js
@@ -81,6 +81,28 @@
 
 // Written as an arrow function
 
-let sum = (num1, num2) => num1 + num2
-let output =sum(10, 7)
-console.log(output)
+// let sum = (num1, num2) => num1 + num2
+// let output =sum(10, 7)
+// console.log(output)
+
+
+// Behavior of "This" Keyword in Arrow functions
+
+// 'This' refers to the owner of the function we are executing. Unlike regular functions, arrow functions do not have their own 'this' value. The value of this is always inherited from the enclosing scope.
+
+
+let message ={
+    name: 'John',
+    regularFunction: function(){
+        console.log(this) //value is the object
+        console.log('Hello ' + this.name)
+    },
+    arrowFunction: () => console.log('Hi ' + this.name) 
+}
+
+message.regularFunction();// prints 'Hello John'
+message.arrowFunction();  //only prints Hi because it can't find name in the scope of the arrow function
+console.log(this) //this logs the entire window?
+
+
+// Changing Function Context & Built-in Functions
